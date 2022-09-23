d8ad887bbd9ea109b49749749f52d96c5ddf52cc
MAHDIA001 <mahdianaiebi@gmail.com>
2022-09-23 21:20:08 +0500

    single page application

--- script.js
+++ script.js
@@ -81,3 +81,27 @@ form.addEventListener('submit', (e) => {
   Book.addBook(book);
   Book.clearFields();
 });
+
+const route = (event) => {
+  event = event  || window.event;
+  event.preventDefault();
+  window.history.pushState({}, '', event.target.href);
+  handleLocation();
+}
+
+const routes = 
+{
+  '/': './index.html',
+  '/list': 'list.html',
+  '/contact': 'contac.html',
+}
+
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  const route  = routes[path];
+  const html = await fetch(route).then((data) => data.text());
+  document.querySelector('.container').innerHTML = html
+}
+window.onpopstate = handleLocation;
+window.route = route;
+handleLocation();
