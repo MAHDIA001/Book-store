57ff00f5e317ae639052468a461fb037a2b3c6a9
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-24 09:22:28 +0430

    Add event listener for page navigation link

--- src/script.js
+++ src/script.js
@@ -1,4 +1,47 @@
-import Book from "./modules/Books.js";
-import Form from "./components/Form.js";
-import List from "./components/List.js";
-import Contact from "./components/Contact.js";
+import Book from './modules/Books.js';
+import Form from './components/Form.js';
+import List from './components/List.js';
+import Contact from './components/Contact.js';
+
+// get DOM of navigation links
+const navLinks = document.querySelectorAll('.nav-link');
+
+// get DOM of page container
+const container = document.getElementById('app');
+
+// load default page
+defaultData();
+
+// add event listener to page navigation links
+navLinks.forEach((element) => {
+  const target = element.getAttribute('data-page');
+  element.addEventListener('click', () => {
+    switch (target) {
+      case 'list':
+        {
+          defaultData();
+        }
+        break;
+      case 'form':
+        {
+          const formData = new Form();
+          container.innerHTML = formData.content;
+        }
+        break;
+      case 'content':
+        {
+          const contentData = new Contact();
+          container.innerHTML = contentData.content;
+        }
+        break;
+      default: {
+        defaultData();
+      }
+    }
+  });
+});
+
+function defaultData() {
+  const listData = new List();
+  container.innerHTML = listData.content;
+}
